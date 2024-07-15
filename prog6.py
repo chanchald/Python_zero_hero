77cbfdc771646e037b414862e30423f8a758a74d
chanchald <66019654+chanchald@users.noreply.github.com>
2024-07-16 00:16:13 +0900

    Add files via upload

---
+++ prog6.py
@@ -0,0 +1,10 @@
+# membership operator
+
+x = "Hello learning python is very easy"
+y = ["Hello", "How", "Are", "You"]
+
+print('h' in x)
+print('h' not in x)
+print('l' in x)
+
+print("Hello" in y)
