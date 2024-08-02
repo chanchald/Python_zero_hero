59e85c73ad7f174f4c81e2ccd161911fbedeedf1
chanchald <deyc2311@gmail.com>
2024-08-02 23:37:33 +0900

    ipass statement for i in range(10):     pass

---
+++ 0020-python.py
@@ -0,0 +1,28 @@
+# pass statement
+# it is a empty statement
+
+# type1
+condition = True
+if condition:
+    pass
+else:
+    print("Condition is False")
+
+# type2
+while False:
+    pass
+
+#type3
+for i in range(10):
+    pass
+
+#try 1
+
+for i in range(100):
+    if i%9==0:
+        print(i)
+    else: pass
+
+
+
+
